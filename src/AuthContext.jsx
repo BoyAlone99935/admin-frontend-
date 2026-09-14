2c3ca0147f6ea9e4449ae35b5069f201cd6f449c
BoyAlone99935 <kingsleychijioke266@gmail.com>
2026-09-14 12:38:38 -0700

    Initial commit

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -17,7 +17,7 @@ export const AuthProvider = ({ children }) => {
       {
           headers: {
   "Content-Type": "application/json",
-  Authorization: `Bearer ${localStorage.getItem("token")}`,
+  Authorization: `Bearer ${localStorage.getItem("adminToken")}`,
 },
       }
     );
